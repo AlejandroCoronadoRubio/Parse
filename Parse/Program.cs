c3d7e4df4849cc8ae18bed75a2a81584c8a645b1
AlejandroCoronadoRubio <32073224+AlejandroCoronadoRubio@users.noreply.github.com>
2020-11-18 21:54:24 -0700

    Add files via upload

---
+++ Parse/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Parse
+{
+    class Program
+    {
+
+        static string ParseSentence(string Sentence)
+        {
+            string[] SeparatedWords = null;
+
+            string Result = string.Empty;
+
+            char Separator = '\0';
+
+            foreach (char Letter in Sentence)
+            {
+                if (!char.IsLetterOrDigit(Letter))
+                {
+                    SeparatedWords = Sentence.Split(Letter);
+                    Separator = Letter;
+                    break;
+                }
+            }
+
+            if (Separator.Equals('\0'))
+            {
+                throw new ArgumentException("To parse the sentence It must contain some special character");
+            }
+
+            foreach (string Word in SeparatedWords)
+            {
+                char FirstLetter = Word[0];
+                char LastLetter = Word[Word.Length-1];
+
+                HashSet<char> WordHashSet = new HashSet<char>(Word.Substring(1, Word.Length - 2));
+
+                Result += $"{FirstLetter}{WordHashSet.Count}{LastLetter}{Separator}";
+            }
+
+            return Result.Substring(0, Result.Length - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ParseSentence("Copyright,Microsoft,Corporation"));
+            Console.WriteLine(ParseSentence("Copyright$Microsoft$Corporation"));
+            Console.WriteLine(ParseSentence("Copyright Microsoft Corporation"));
+            Console.WriteLine(ParseSentence("CopyrightMicrosoftCorporation")); //This one is going to throw an exception
+        }
+    }
+}
